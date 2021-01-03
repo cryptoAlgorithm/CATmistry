a0cba5be7acba1fb015c5c9d8147c69e4f3de20f
CryptoAlgo-Dev <64193267+cryptoAlgorithm@users.noreply.github.com>
2021-01-03 21:26:10 +0800

    Completed Learn homepage layout, working on Play now

---
+++ app/src/main/java/com/catmistry/android/SquareImageView.kt
@@ -0,0 +1,23 @@
+package com.catmistry.android
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+
+class SquareImageView : androidx.appcompat.widget.AppCompatImageView {
+    constructor(context: Context?) : super(context!!) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context!!,
+        attrs,
+        defStyleAttr
+    ) {
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+}
