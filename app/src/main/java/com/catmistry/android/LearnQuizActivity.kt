b3b7f11fe0a496b9cd5810484af5abae9ab2939f
CryptoAlgo-Dev <64193267+cryptoAlgorithm@users.noreply.github.com>
2021-01-10 17:56:06 +0800

    Fix some timer issues and improved bottom sheet layout

--- app/src/main/java/com/catmistry/android/LearnQuizActivity.kt
+++ app/src/main/java/com/catmistry/android/LearnQuizActivity.kt
@@ -2,13 +2,11 @@ package com.catmistry.android
 
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.DataSnapshot
@@ -34,19 +32,27 @@ class LearnQuizActivity : AppCompatActivity() {
         if (timeLeft == null) timeLeft = totalTime
 
         val runnable = Runnable {
-            while (continueTimer) {
+            while (continueTimer && !Thread.interrupted()) {
                 val newProg = ((timeLeft!! / totalTime) * 100.0).toInt()
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     progressBar.setProgress(newProg, true)
                 }
                 else progressBar.progress = newProg
 
+                if (timeLeft!! <= 0.0) {
+                    runOnUiThread {
+                        // Stuff that updates the UI
+                        // Android how I hate you
+                        checkAns(5, questionsArray[0]?.correctAnswer!!) // This will never be right
+                    }
+                    break
+                }
+
                 timeLeft = timeLeft!! - 50
 
                 Thread.sleep(50)
             }
 
-            Toast.makeText(this@LearnQuizActivity, "ayo", Toast.LENGTH_SHORT).show()
             timeLeft = null
             continueTimer = false
         }
@@ -55,6 +61,13 @@ class LearnQuizActivity : AppCompatActivity() {
     }
 
     private fun showNextQn() {
+        // Gracefully stop konfetti
+        konfettiView.stopGracefully()
+
+        // Interrupt timer
+        currentTimerThread?.interrupt()
+        timeLeft = null
+
         // Delete shown question (or fail silently)
         questionsArray.removeFirstOrNull()
 
@@ -62,8 +75,6 @@ class LearnQuizActivity : AppCompatActivity() {
             Toast.makeText(this@LearnQuizActivity, "Quiz complete", Toast.LENGTH_SHORT).show()
             return
         }
-        continueTimer = true
-        startTimer(10000.0)
         updateUI(questionsArray[0])
 
         // Enable buttons once again
@@ -71,21 +82,36 @@ class LearnQuizActivity : AppCompatActivity() {
         optTwo.isEnabled   = true
         optThree.isEnabled = true
         optFour.isEnabled  = true
+
+        continueTimer = true
+        startTimer(10000.0)
     }
 
     private fun checkAns(enteredAns: Int, correctAns: Int) {
         // Pause the timer
         continueTimer = false
-        currentTimerThread?.interrupt()
-        timeLeft = null
 
         if (enteredAns == correctAns) {
             resultImg.setImageResource(R.drawable.check)
             resultHeader.text = getString(R.string.quiz_result_correct)
+            resultSubtitle.text = getString(R.string.correct_ans_correct, (questionsArray.size - 1).toString())
+            konfettiView.build()
+                    .addColors(Color.rgb(237, 103, 83), Color.rgb(97, 198, 164),
+                            Color.rgb(245, 205, 165), Color.rgb(86, 192, 213),
+                            Color.rgb(150, 100, 132))
+                    .setDirection(0.0, 180.0)
+                    .setSpeed(0.8f, 1.2f)
+                    .setFadeOutEnabled(true)
+                    .setTimeToLive(100000L)
+                    .addShapes(Shape.Square, Shape.Circle)
+                    .addSizes(Size(10))
+                    .setPosition(-50f, konfettiView.width + 10000f, -50f, 0f)
+                    .streamFor(200, StreamEmitter.INDEFINITE)
         }
         else {
             resultImg.setImageResource(R.drawable.close)
             resultHeader.text = getString(R.string.quiz_result_wrong, (questionsArray.size - 1).toString())
+            resultSubtitle.text = getString(R.string.correct_ans_wrong, questionsArray[0]?.options?.get(correctAns - 1))
         }
 
         BottomSheetBehavior.from(resultsSheet).state = BottomSheetBehavior.STATE_EXPANDED
@@ -120,8 +146,16 @@ class LearnQuizActivity : AppCompatActivity() {
         }
     }
 
+    private fun resumeTimer() {
+        continueTimer = true // Resume timer
+        startTimer(10000.0)
+    }
+
     override fun onBackPressed() {
+        val prevState = continueTimer
+
         continueTimer = false // Pause timer if running
+
         MaterialAlertDialogBuilder(this)
             .setTitle(resources.getString(R.string.end_quiz_title))
             .setMessage(resources.getString(R.string.end_quiz_body))
@@ -132,12 +166,14 @@ class LearnQuizActivity : AppCompatActivity() {
             }
             .setPositiveButton(resources.getString(R.string.end_quiz_no)) { _, _ ->
                 // Respond to positive button press
-                continueTimer = true // Resume timer
-                startTimer(10000.0)
+                if (prevState) {
+                    resumeTimer()
+                }
             }
             .setOnCancelListener {
-                continueTimer = true // Resume timer
-                startTimer(10000.0)
+                if (prevState) {
+                    resumeTimer()
+                }
             }
             .show()
     }
@@ -146,21 +182,12 @@ class LearnQuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn_quiz)
 
-        BottomSheetBehavior.from(resultsSheet).state = BottomSheetBehavior.STATE_HIDDEN
+        val bottomSheet = BottomSheetBehavior.from(resultsSheet)
+
+        bottomSheet.state = BottomSheetBehavior.STATE_HIDDEN
 
         // Start konfetti
-        /* konfettiView.build()
-            .addColors(Color.rgb(237, 103, 83), Color.rgb(97, 198, 164),
-                Color.rgb(245, 205, 165), Color.rgb(86, 192, 213),
-                Color.rgb(150, 100, 132))
-            .setDirection(0.0, 180.0)
-            .setSpeed(0.8f, 1.2f)
-            .setFadeOutEnabled(true)
-            .setTimeToLive(100000L)
-            .addShapes(Shape.Square, Shape.Circle)
-            .addSizes(Size(10))
-            .setPosition(-50f, konfettiView.width + 10000f, -50f, 0f)
-            .streamFor(200, StreamEmitter.INDEFINITE) */
+        /*  */
 
         Toast.makeText(this, intent.extras?.getString("quizTopic"), Toast.LENGTH_SHORT).show()
 
@@ -169,7 +196,6 @@ class LearnQuizActivity : AppCompatActivity() {
                 dataSnapshot.children.forEach {
                     val listData = it.getValue<LearnQns>()
                     questionsArray.add(listData)
-                    Log.e("List data", listData.toString())
                 }
                 showNextQn()
             }
@@ -191,7 +217,12 @@ class LearnQuizActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        BottomSheetBehavior.from(resultsSheet).addBottomSheetCallback(object :BottomSheetBehavior.BottomSheetCallback() {
+        sheetEndButton.setOnClickListener {
+            bottomSheet.state = BottomSheetBehavior.STATE_HIDDEN
+            showNextQn()
+        }
+
+        bottomSheet.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                     showNextQn()
